9b1a2ac64b093debaea20506b3b8a520a871e22a
Salsabila Putri <74218691+salsabilapl@users.noreply.github.com>
2023-12-22 04:05:29 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from google_play_scraper import app
 import pandas as pd
 import numpy as np
 import joblib
-from plotly import express as px
+import plotly as px
 import nltk
 from nltk.corpus import stopwords
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
