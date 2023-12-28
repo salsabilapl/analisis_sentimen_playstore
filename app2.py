319625f398febe64fbcc965fb3207a7ff9c030cd
Salsabila Putri <74218691+salsabilapl@users.noreply.github.com>
2023-12-28 22:27:28 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -71,7 +71,7 @@ def main():
         st.markdown("""
             ### Selamat Datang di Aplikasi Sentiment Analysis
             """)
-        st.image('sentiment-icon.png', use_column_width=10)
+        st.image('sentiment-icon.png', use_column_width=1000)
         st.markdown("""Aplikasi ini menganalisis ribuan ulasan dari Google Play Store secara real-time untuk memberikan wawasan yang mendalam mengenai sentimen pengguna terhadap aplikasi yang Anda pilih.""")
         st.markdown("""[Silakan pilih 'Sentiment Analysis' di sidebar untuk melakukan analisis sentimen ulasan aplikasi.]""")
     elif menu=='🤖 Sentiment Analysis':
